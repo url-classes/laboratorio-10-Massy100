5015a11a974be190f4fd0c0f6c7207191f42a430
Massy100 <80069634+Massy100@users.noreply.github.com>
2024-11-08 09:20:27 -0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -8,4 +8,18 @@ myRBTree.insert(20);
 myRBTree.insert(30);
 myRBTree.insert(50);
 myRBTree.insert(45);
+
+if (myRBTree.search(11) !== null) {
+    console.log("Dato encontrado");
+} else {
+    console.log("Dato no encontrado");
+}
+if (myRBTree.search(10) !== null) {
+    console.log("Dato encontrado");
+} else {
+    console.log("Dato no encontrado");
+}
 myRBTree.printAll();
+
+console.log("Impresion en order ascendente:")
+myRBTree.inOrderPrint();
